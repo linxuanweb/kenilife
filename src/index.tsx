78324b342333648cae4edbe065bd5e3c9d54bf50
linxuan <linxuan.li.work@outlook.com>
2022-12-02 18:46:55 +0800

    [ts-migrate][.] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// @ts-expect-error TS(2345): Argument of type 'HTMLElement | null' is not assig... Remove this comment to see the full error message
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
   <React.StrictMode>
+    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <App />
   </React.StrictMode>
 );
@@ -14,4 +17,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// @ts-expect-error TS(2554): Expected 1 arguments, but got 0.
 reportWebVitals();
