d0ce707ead5c9baa522259564960d1faadb93469
linxuan <linxuan.li.work@outlook.com>
2022-12-02 18:57:01 +0800

    Add typescript

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+import "@arco-design/web-react/dist/css/arco.css";
+import React from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="/imgs/logo.png" className="App-logo" alt="likeni" />
+
+                <p>
+                    李柯妮的个人网站（coming soon...）
+                </p>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 // @ts-expect-error TS(2345): Argument of type 'HTMLElement | null' is not assig... Remove this comment to see the full error message
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
   <React.StrictMode>
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <App />
   </React.StrictMode>
 );
